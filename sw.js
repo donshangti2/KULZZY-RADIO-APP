e6fdbdb1ea0bc7ff4959ae881cb814c07d04466d
Chukwuma Peter Chinedu <donshangti2@gmail.com>
2026-08-31 10:29:16 +0100

    Update service worker to version 2 and improve caching

--- sw.js
+++ sw.js
@@ -1,53 +1,91 @@
-const CACHE_NAME = "kulzzy-radio-v1";
+const CACHE_NAME = "kulzzy-radio-v2";
 
 const APP_SHELL = [
   "./",
+  "./index.html",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
   "./logo.jpg"
 ];
 
-self.addEventListener("install", (event) => {
+
+/* INSTALL NEW VERSION */
+
+self.addEventListener("install", event => {
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+
+    caches.open(CACHE_NAME).then(cache => {
+
       return cache.addAll(APP_SHELL);
+
     })
+
   );
 
   self.skipWaiting();
+
 });
 
-self.addEventListener("activate", (event) => {
+
+/* DELETE OLD VERSIONS */
+
+self.addEventListener("activate", event => {
+
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+
+    caches.keys().then(cacheNames => {
+
       return Promise.all(
+
         cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
+
+          .filter(name => name !== CACHE_NAME)
+
+          .map(name => caches.delete(name))
+
       );
+
     })
+
   );
 
   self.clients.claim();
+
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+
+/* ALWAYS CHECK INTERNET FIRST */
+
+self.addEventListener("fetch", event => {
+
+  if(event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const responseClone = response.clone();
 
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseClone);
+    fetch(event.request)
+
+      .then(response => {
+
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+
+          cache.put(event.request, copy);
+
         });
 
         return response;
+
       })
+
       .catch(() => {
+
         return caches.match(event.request);
+
       })
+
   );
+
 });
